d5d38dbdfbb5b98d0754b105ecd390247c48ad81
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-02-25 21:21:51 +0200

    Added Home

---
+++ TaskSwiftUI/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  TaskSwiftUI
+//
+//  Created by Sergey Lukaschuk on 25.02.2021.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        
+        Button(action: {}) {
+            Text("")
+        }
+    }
+}
+
